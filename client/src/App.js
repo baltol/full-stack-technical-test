d37fa8a4f4c128288b0677d9fa1fddc0e5d66f6e
baltol <smith@example.com>
2019-08-17 15:47:47 -0400

    Test requirements
      * Task 1: Render data in table
      * Task 2: format numbers with libphonenumbers-js
      * Task 3: add pagination

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card, Table } from 'antd'
 import './App.css'
+import { format, columns } from './lib.js'
+
+const BASE_URL = 'http://localhost:3000/api/data';
+const url = ({ current, pageSize }) => `${BASE_URL}?current=${current}&pageSize=${pageSize}`;
 
 function App() {
-  return (
+    const [data, setData] = useState([]);
+    const [count, setCount] = useState(0);
+
+    const fetchNumbers = ({ current, pageSize } = {}) => {
+        if (!current || !pageSize) {
+            current = 1;
+            pageSize = 5;
+        }
+        return fetch(url({current, pageSize}))
+            .then(res => res.json())
+            .then(array => {
+                const [doc] = array;
+                setData(array.map(format));
+                setCount(doc.count);
+            })
+            .catch(error => console.error(error));
+    };
+
+    useEffect(() => {
+        fetchNumbers();
+    }, []);
+
+    return (
     <main className="App">
       <section className="App-header">
         <h1>Numbers</h1>
@@ -16,7 +42,12 @@ function App() {
           title="Available Numbers"
           bordered={true}
         >
-          <Table />
+        <Table
+          dataSource={data}
+          columns={columns}
+          onChange={fetchNumbers}
+        pagination={{ pageSize: 5, total: count, defaultCurrent: 1 }}
+        />
         </Card>
       </section>
     </main>
